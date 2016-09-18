3abe207f047b056cbd9268c2ec1204923d0f16aa
Dani Adkins <danirenaeadkins12@gmail.com>
2016-09-18 12:29:12 -0500

    initial commit

---
+++ iife-xhr.js
@@ -0,0 +1,12 @@
+console.log("Hey!");
+
+function showCarnivores (carnivores) {
+
+}
+
+function showHerbivores (herbivores) {
+
+}
+
+// Predator.loadCarnivores(showCarnivores);
+// Predator.loadHerbivores(showHerbivores);
